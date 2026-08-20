c8d83ac554a875a6a8f28b7fe172fc906ffb5983
Matheus Reis <47448206+matheusoreis@users.noreply.github.com>
2026-08-20 17:42:32 -0300

    Constantes

--- server/source/common/constants.gd
+++ server/source/common/constants.gd
@@ -1,1 +1,34 @@
 extends Node
+
+
+const MAPS_DATA_DIRECTORY: String = ""
+
+
+const NETWORK_HOST: String = "0.0.0.0"
+const NETWORK_PORT: int = 7001
+
+
+const MAX_PEERS: int = 100
+
+
+const CELL_SIZE: int = 32
+
+const CELL_NONE: int = 0
+const CELL_FULL_BLOCK: int = 1
+const CELL_TOP: int = 2
+const CELL_RIGHT: int = 4
+const CELL_DOWN: int = 8
+const CELL_LEFT: int = 16
+
+
+const START_MAP: int = 0
+const START_MAP_POSITION: Vector2i = Vector2i(68, 64)
+const START_MAP_FACING: Vector2i = Vector2i.DOWN
+
+
+const AVALIABLE_SPRITES: Array[String] = ["red"]
+
+
+const IDENTIFIER_REGEX: String = ""
+const EMAIL_REGEX: String = ""
+const PASSWORD_REGEX: String = ""
