a8c0b8219884134f295afacb787dcceb1a949ded
Matheus Reis <47448206+matheusoreis@users.noreply.github.com>
2026-08-21 18:29:37 -0300

    Ajustes no nome correto e base das scenes

--- client/source/interfaces/interface.gd
+++ client/source/interfaces/interface.gd
@@ -1,5 +1,5 @@
 extends Control
-class_name container
+class_name Interface
 
 
 func _ready() -> void:
--- client/source/scenes/scene.gd
+++ client/source/scenes/scene.gd
@@ -1,2 +1,63 @@
 extends Node2D
 class_name Scene
+
+
+@export_category("Interfaces")
+@export var _interfaces: Dictionary[StringName, Interface]
+
+
+func _ready() -> void:
+	Globals.current_scene = self
+
+
+func add_interface(identifier: StringName, interface: Interface) -> void:
+	if is_instance_valid(interface) == false:
+		return
+
+	if _interfaces.has(identifier):
+		return
+
+	_interfaces[identifier] = interface
+
+
+func remove_interface(identifier: StringName) -> void:
+	var interface: Interface = get_interface(identifier)
+	if interface == null:
+		return
+
+	_interfaces.erase(identifier)
+	interface.queue_free()
+
+
+func get_interface(identifier: StringName) -> Interface:
+	return _interfaces.get(identifier, null)
+
+
+func toggle_interface(identifier: StringName) -> void:
+	var interface: Interface = get_interface(identifier)
+	if interface == null:
+		return
+
+	interface.visible = not interface.visible
+
+	if interface.visible and interface is Interface:
+		(interface as Interface).bring_to_front()
+
+
+func show_interface(identifier: StringName) -> void:
+	var interface: Interface = get_interface(identifier)
+	if interface == null:
+		return
+
+	interface.visible = true
+
+	if interface is Interface:
+		(interface as Interface).bring_to_front()
+
+
+func hide_interface(identifier: StringName) -> void:
+	var interface: Interface = get_interface(identifier)
+	if interface == null:
+		return
+
+	interface.visible = false
