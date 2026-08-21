bd868a9f8fb2f73e08a917081eec611979e53222
Matheus Reis <47448206+matheusoreis@users.noreply.github.com>
2026-08-20 23:36:20 -0300

    Modelos do banco de dados

--- server/source/database/models.gd
+++ server/source/database/models.gd
@@ -5,3 +5,37 @@ class_name Models
 func from_row(row: Array, cols: PackedStringArray):
 	for i in cols.size():
 		set(cols[i], row[i])
+
+
+class AccountModel extends Models:
+	var id: int
+
+	var email: String
+	var password: String
+
+	var access_at: int
+
+	var created_at: int
+	var updated_at: int
+
+
+class CharacterModel extends Models:
+	var id: int
+
+	var account: int
+
+	var identifier: String
+	var spritesheet: String
+
+	var map: int
+
+	var cel_x: int
+	var cel_y: int
+
+	var facing_x: int
+	var facing_y: int
+
+	var access_at: int
+
+	var created_at: int
+	var updated_at: int
