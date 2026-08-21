fa1f7311a9554b5764d1238aa7157abd51057116
Matheus Reis <47448206+matheusoreis@users.noreply.github.com>
2026-08-20 22:20:03 -0300

    Network adicionado

---
+++ server/source/network/network.gd
@@ -0,0 +1,52 @@
+extends Node
+class_name Network
+
+
+var network: Multiplayer.Server
+
+
+func _ready() -> void:
+	network = Multiplayer.Server.new()
+
+
+func start() -> Error:
+	return network.start(Constants.NETWORK_HOST, Constants.NETWORK_PORT, Constants.MAX_PEERS)
+
+
+func stop() -> Error:
+	return network.stop()
+
+
+func exec(target: Variant, fn_path: StringName, args: Array = []) -> Error:
+	return network.exec(target, fn_path, args)
+
+
+func get_peers() -> Array[int]:
+	return network.get_peers()
+
+
+func get_peer_count() -> int:
+	return network.get_peer_count()
+
+
+func has_peer(peer_id: int) -> bool:
+	return network.has_peer(peer_id)
+
+
+func sender_id() -> int:
+	return network.sender_id()
+
+
+func peer_address(peer_id: int) -> String:
+	return network.peer_address(peer_id)
+
+
+func kick(peer_id: int) -> void:
+	network.kick(peer_id)
+
+
+func _physics_process(_delta: float) -> void:
+	if network == null:
+		return
+
+	network.poll()
