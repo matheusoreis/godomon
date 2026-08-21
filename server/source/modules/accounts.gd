48d9cfc7a12027301aa6ba386b0844e75167af17
Matheus Reis <47448206+matheusoreis@users.noreply.github.com>
2026-08-21 09:59:16 -0300

    Módulo da conta

---
+++ server/source/modules/accounts.gd
@@ -0,0 +1,34 @@
+extends Node
+
+
+var _accounts: Dictionary[int, Account] = {}
+
+
+func add(peer_id: int, account: Account) -> void:
+	if has(peer_id):
+		return
+
+	_accounts[peer_id] = account
+
+
+func remove(peer_id: int) -> void:
+	if not has(peer_id):
+		return
+
+	_accounts.erase(peer_id)
+
+
+func account(peer_id: int) -> Account:
+	return _accounts.get(peer_id)
+
+
+func has(peer_id: int) -> bool:
+	return _accounts.has(peer_id)
+
+
+func count() -> int:
+	return _accounts.size()
+
+
+func all() -> Array[Account]:
+	return _accounts.values()
