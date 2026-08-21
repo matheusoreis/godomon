c9ce6791301bbbab007fdc3202c047bcdd2c2152
Matheus Reis <47448206+matheusoreis@users.noreply.github.com>
2026-08-21 16:19:03 -0300

    Row para direção de cada sprite

--- client/source/common/constants.gd
+++ client/source/common/constants.gd
@@ -1,1 +1,28 @@
 extends Node
+
+
+const NETWORK_HOST: String = "127.0.0.1"
+const NETWORK_PORT: int = 7001
+
+
+const CELL_SIZE: int = 32
+
+const CELL_NONE: int = 0
+const CELL_FULL_BLOCK: int = 1
+const CELL_UP: int = 2
+const CELL_RIGHT: int = 4
+const CELL_DOWN: int = 8
+const CELL_LEFT: int = 16
+
+
+const DIRECTION_SPRITE_ROW: Dictionary[Vector2i, int] = {
+	Vector2i.DOWN: 0,
+	Vector2i.LEFT: 2,
+	Vector2i.RIGHT: 3,
+	Vector2i.UP: 1,
+}
+
+
+const IDENTIFIER_REGEX: String = ""
+const EMAIL_REGEX: String = ""
+const PASSWORD_REGEX: String = ""
