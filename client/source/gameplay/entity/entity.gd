e92697defab7661bcbc5b7ce642b07a8a288a898
Matheus Reis <47448206+matheusoreis@users.noreply.github.com>
2026-08-22 10:45:58 -0300

    Correção da entidade

--- client/source/gameplay/entity/entity.gd
+++ client/source/gameplay/entity/entity.gd
@@ -1,2 +1,30 @@
 extends Node2D
 class_name Entity
+
+
+var id: int
+var identifier: String
+
+var spritesheet: String
+
+var map: int
+var cell: Vector2i
+var facing: Vector2i
+
+
+func _init(
+	id: int,
+	identifier: String,
+	spritesheet: String,
+	map: int,
+	cell: Vector2i,
+	facing: Vector2i
+) -> void:
+	self.id = id
+	self.identifier = identifier
+
+	self.spritesheet = spritesheet
+
+	self.map = map
+	self.cell = cell
+	self.facing = facing
